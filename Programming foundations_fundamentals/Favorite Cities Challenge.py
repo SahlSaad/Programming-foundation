848a7e4202703cb7f97bef9b11759107e3852c07
SahlSaad <89551325+SahlSaad@users.noreply.github.com>
2021-09-05 11:38:04 +0300

    Add files via upload

---
+++ Programming foundations_fundamentals/Favorite Cities Challenge.py
@@ -0,0 +1,15 @@
+#Favorite Cities Challenge
+
+
+# Make a program that prints out the name of your favorite cities, with the following requirements :
+# Has a function named favorite_city
+# favorite_city should have one parameter: name
+# You chould call favorite_city at least three times
+# the output should include: "One of my favorite cities is"
+
+def favorite_city(name):
+    print("One of my favorite cities is", name)
+
+favorite_city("mecca, Saudi Arabia")
+favorite_city("Istanbul, Turkey")
+favorite_city("Dubai, Arab Emirates")
---
+++ Programming foundations_fundamentals/Guessing Game Challenge.py
@@ -0,0 +1,23 @@
+# Guessing Game Exercise
+
+# Write a program that makes the following:
+# Ask the user: "What's my favorite food"
+# If the user enters the name of your favorite food, output: "Yep! So amazing!"
+# If the user doesn't enter the name of your favorite food, output: "Yuck! That's not it"
+# Regardless of what the user enters, output: "Thanks for playing!"
+
+
+# val = input("Enter your value: ")
+# print(val)
+
+# exit()
+
+fav_food = input("What's my favorite food?")
+print(fav_food)
+
+if fav_food == "pizza":
+    print("Yep! So amazing!")
+else :
+    print("Yuck! That's not it")
+
+print("Thanks for playing!")
